9a380fb0f5f6f3f9f6166fde092e7da83419bd47
jyb-maker <bjy7328@naver.com>
2024-12-12 16:20:42 +0900

    mvb_board 예제

---
+++ src/main/java/com/jbedu/board/controller/BoardController.java
@@ -0,0 +1,49 @@
+package com.jbedu.board.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("*.do")  // Controller 기능의 클래스로 서버가 인식하도록 함 
+public class BoardController extends HttpServlet{
+
+	public BoardController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 중요한 건 매개변수!!
+		System.out.println("doGet 호출");
+		request.setCharacterEncoding("utf-8");		
+
+		String viewPage = "list.jsp"; //글 목록 페이지 파일 이름
+				
+		String uri = request.getRequestURI();  //http://localhost:8888/jsp_mvc_board-20241212_jy/list.jsp
+		String conPath = request.getContextPath();  //http://localhost:8888
+		String com = uri.substring(conPath.length()); // uri-conPath(길이) -> jsp_mvc_board-20241212_jy/list.jsp
+		
+		System.out.println(uri);
+		System.out.println(conPath);
+		System.out.println(com);
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
+		dispatcher.forward(request, response);
+				
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 중요한 건 매개변수!!
+		 
+	}
+
+
+	
+}
---
+++ src/main/webapp/main.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>main화면</title>
+</head>
+<body>
+	<h1>메인 화면</h1>
+	<hr>
+	<a href="list.do"> 모든 게시판 글 보기 </a>
+
+</body>
+</html>
